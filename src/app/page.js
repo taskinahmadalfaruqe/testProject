ee63bc4e3f5fdfa80f469b7d190d863617053df1
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-15 12:55:27 +0600

    added copy write in button component

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       testing by taskin
-<<<<<<< HEAD
       <Button />
-=======
       <p>Development Branch</p>
->>>>>>> c7a81872ac82a6e8059d05865bcded1f1ca81a2d
     </main>
   );
 }
