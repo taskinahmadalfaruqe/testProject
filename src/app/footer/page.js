73961d7b9b62c00d0146ef29137e1968230b61f0
Rajib <rajib@ubuntu>
2024-01-15 14:23:27 +0600

    footer added by rajib

---
+++ src/app/footer/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const footerPage = () => {
+    return (
+        <div>
+            <footer>
+                all reserver &copy; by code riders
+            </footer>
+        </div>
+    );
+};
+
+export default footerPage;
